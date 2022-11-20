b2ec23210a95fb61acaa71a88a0076f4c479719e
Vishwanatharaddi <vishwanathvr7@gmail.com>
2022-11-20 21:15:20 +0530

    adding the dom.js file

--- dom.js
+++ dom.js
@@ -72,15 +72,90 @@
 //secondItem.style.color = 'coral';
 
 //QUEARYSELECTOR//
-var titles = document.querySelectorAll('.title');
+//var titles = document.querySelectorAll('.title');
 
-console.log(titles);
-titles[0].textContent = 'Hello';
+//console.log(titles);
+//titles[0].textContent = 'Hello';
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
+//var odd = document.querySelectorAll('li:nth-child(odd)');
  
-for(var i =0; i< odd.length; i++){
-    odd[i].style.backgroundColor = 'green'
-}
-   
+//for(var i =0; i< odd.length; i++){
+   // odd[i].style.backgroundColor = 'green'
+//}
 
+
+
+ // TRAVERSING DOM
+ var itemList = document.querySelector('#items');
+  //Parentnode
+  //console.log(itemList.parentNode);
+  //itemList.parentNode.style.backgroundColor = 'lightgrey';
+  //console.log(itemList.parentNode.parentNode);
+
+   //ParentElement
+   //console.log(itemList.parentElement);
+   //itemList.parentElement.style.backgroundColor = 'lightgrey';
+   //console.log(itemList.parentElement.parentElement);
+ 
+   // childNodes
+  // console.log(itemList.childNodes);
+
+ // console.log(itemList.childern);
+ // console.log(itemList.children[1]);
+  //itemList.children[1].style.backgroundColor = 'skyblue';
+
+//firstChild
+//console.log(itemList.firstChild);
+
+//firstElementChild
+//console.log(itemList.firstElementChild);
+//itemList.firstElementChild.textContent = 'Hello 1';
+
+//lastChild
+//console.log(itemList.lastChild);
+
+//lastElementChild
+//console.log(itemList.lastElementChild);
+//itemList.lastElementChild.textContent = 'Hello 5';
+
+//nextSibiling
+//console.log(itemList.nextSibiling);
+
+//nextElementSibiling
+//console.log(itemList.nextElementSibiling);
+
+//previousSibiling
+//console.log(itemList.previousSibiling);
+
+//previousElementSibiling
+//console.log(itemList.previousElementSibiling);
+//itemList.previousElementSibiling.style.color = 'green';
+
+//createElement
+
+// Crate a div
+var newDiv = document.createElement('div');
+
+//Add class
+newDiv.className ='hello';
+
+//Add id
+newDiv.id ='hello1';
+
+//Add attr
+newDiv.setAttribute ('title','Hello Div');
+
+//Create text mode
+var newDivText = document.createTextNode('Hello world');
+
+//Add text to div
+newDiv.appendChild(newDivText);
+
+var container =  document.querySelector('header .container');
+var h1 = document.querySelector('header h1')
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '30px'
+
+container.insertBefore(newDiv, h1);
