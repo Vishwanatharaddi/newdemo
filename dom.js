ac576de1646c62db06d3e9e60d0a41201dab03ad
Vishwanatharaddi <vishwanathvr7@gmail.com>
2022-11-20 17:37:42 +0530

    adding second time dom.js

--- dom.js
+++ dom.js
@@ -51,25 +51,36 @@
 //li[2].style.backgroundColor = 'green';
 
 //QUEARYSELECTOR//
-var header = document.querySelector('#main-header');
-header.style.borderBottom = 'solid 4px #ccc';
+//var header = document.querySelector('#main-header');
+//header.style.borderBottom = 'solid 4px #ccc';
 
-var input = document.querySelector('input');
-input.value = 'Hello World'
+//var input = document.querySelector('input');
+//input.value = 'Hello World'
 
-var submit = document.querySelector('input[type="submit"]');
-submit.value="SEND"
+//var submit = document.querySelector('input[type="submit"]');
+//submit.value="SEND"
 
-var item = document.querySelector('.list-group-item');
-item.style.color="red"
+//var item = document.querySelector('.list-group-item');
+//item.style.color="red"
 
-var LastItem = document.querySelector
-('.list-group-item:last-child');
-LastItem.style.color = 'blue';
+//var LastItem = document.querySelector
+//('.list-group-item:last-child');
+//LastItem.style.color = 'blue';
 
-var secondItem = document.querySelector
-('.list-group-item:nth-child(2)');
-secondItem.style.color = 'coral';
+//var secondItem = document.querySelector
+//('.list-group-item:nth-child(2)');
+//secondItem.style.color = 'coral';
 
+//QUEARYSELECTOR//
+var titles = document.querySelectorAll('.title');
+
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+ 
+for(var i =0; i< odd.length; i++){
+    odd[i].style.backgroundColor = 'green'
+}
    
 
